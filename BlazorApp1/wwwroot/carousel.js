74930bc94709055981dd9ccc13794372da7aa845
Nawa Mumbwe <mumbwe94@gmail.com>
2021-11-27 20:00:17 +0200

    fix route

---
+++ BlazorApp1/wwwroot/carousel.js
@@ -0,0 +1,26 @@
+﻿function swicthModal() {
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('slider', () => ({
+            currentIndex: 1,
+            images: [
+                'https://source.unsplash.com/1600x900/?beach',
+                'https://source.unsplash.com/1600x900/?cat',
+                'https://source.unsplash.com/1600x900/?dog',
+                'https://source.unsplash.com/1600x900/?lego',
+                'https://source.unsplash.com/1600x900/?textures&patterns'
+            ],
+            back() {
+                if (this.currentIndex > 1) {
+                    this.currentIndex = this.currentIndex - 1;
+                }
+            },
+            next() {
+                if (this.currentIndex < this.images.length) {
+                    this.currentIndex = this.currentIndex + 1;
+                } else if (this.currentIndex <= this.images.length) {
+                    this.currentIndex = this.images.length - this.currentIndex + 1
+                }
+            },
+        }))
+    })
+}
