7e5f0dffbd4b7765427d6223c3c79e195b5bbc97
Nawa Mumbwe <mumbwe94@gmail.com>
2021-11-27 13:01:15 +0200

    blazor wasm app.

---
+++ modal.js
@@ -0,0 +1,8 @@
+﻿function OpenModal(modal) {
+    modal.style.display = "block";
+}
+
+// We want the modal to close when the OK button is clicked
+function CloseModal(modal) {
+    modal.style.display = "none";
+}
